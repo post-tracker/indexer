4d0f382fb2f7281915acb0e4d645c2a26b203cd5
Oskar Risberg <kokarn@gmail.com>
2017-03-21 20:57:14 +0100

    Remove Post and make a isValidPost module

---
+++ src/modules/isValidPost.js
@@ -0,0 +1,31 @@
+const isValidPost = function isValidPost ( postData, filterData ) {
+    if ( postData.content.length <= 0 ) {
+        return false;
+    }
+
+    // Filter for specific forums if we want
+    if ( filterData && filterData.matchOnly ) {
+        if ( !Array.isArray( filterData.matchOnly ) ) {
+            filterData.matchOnly = [ filterData.matchOnly ];
+        }
+
+        if ( filterData.matchOnly.indexOf( postData.section ) === -1 ) {
+            return false;
+        }
+    }
+
+    // Filter for specific forums if we want
+    if ( filterData && filterData.exclude ) {
+        if ( !Array.isArray( filterData.exclude ) ) {
+            filterData.exclude = [ filterData.exclude ];
+        }
+
+        if ( filterData.exclude.indexOf( postData.section ) > -1  ) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+module.exports = isValidPost;
