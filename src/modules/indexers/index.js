915d68a955242cfaec205f4d996c06c643fa8b83
Oskar Risberg <oskar.risberg@stendahls.se>
2017-12-14 18:30:38 +0100

    Disable reddit indexer

--- src/modules/indexers/index.js
+++ src/modules/indexers/index.js
@@ -3,7 +3,7 @@ module.exports = {
     'Bungie.net': require( './BungieNet.js' ),
     InvisionPowerBoard: require( './InvisionPowerBoard.js' ),
     MiggyRSS: require( './MiggyRSS.js' ),
-    Reddit: require( './Reddit.js' ),
+    // Reddit: require( './Reddit.js' ),
     SimpleMachinesForum: require( './SimpleMachinesForum.js' ),
     Steam: require( './Steam.js' ),
     Twitter: require( './Twitter.js' ),
