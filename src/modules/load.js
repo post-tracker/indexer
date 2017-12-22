a299074468ecf0b48799eea417def3ad2d6e5180
Oskar Risberg <oskar.risberg@stendahls.se>
2017-12-22 20:10:07 +0100

    Increase default timeout

--- src/modules/load.js
+++ src/modules/load.js
@@ -126,7 +126,7 @@ class Load {
             response = await got( url,
                 {
                     headers: headers,
-                    timeout: 10000,
+                    timeout: 20000,
                 }
             );
         } catch ( urlLoadError ) {
