d9a2605346349f379dea6821da25813f4b18ee4d
Oskar Risberg <oskar.risberg@stendahls.se>
2026-06-05 20:29:24 +0200

    Remove dead MiggyRSS and Reddit indexers
    MiggyRSS's feed is dead; Reddit indexing moved to the queue-users/grunt/peon
    pipeline. Both were already unused (Reddit was commented out); drop the
    registry entries and the indexer classes.

--- modules/indexers/index.js
+++ modules/indexers/index.js
@@ -2,8 +2,6 @@
 module.exports = {
     'Bungie.net': require( './BungieNet.js' ),
     InvisionPowerBoard: require( './InvisionPowerBoard.js' ),
-    MiggyRSS: require( './MiggyRSS.js' ),
-    // Reddit: require( './Reddit.js' ),
     SimpleMachinesForum: require( './SimpleMachinesForum.js' ),
     Steam: require( './Steam.js' ),
     Twitter: require( './Twitter.js' ),
